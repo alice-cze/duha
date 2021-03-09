b1821d0d56747f13d07dc90dbe231cf886c69e7f
alice-cze <80326251+alice-cze@users.noreply.github.com>
2021-03-09 18:47:47 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    strip.clear()
-})
-input.onButtonPressed(Button.AB, function () {
     strip.showRainbow(1, 360)
 })
 input.onButtonPressed(Button.B, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    strip.showColor(neopixel.rgb(255, 103, 23))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    strip.clear()
 })
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
+strip.setBrightness(255)
 basic.forever(function () {
     strip.rotate(1)
     basic.pause(200)
