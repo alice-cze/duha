615215e3f6bedf347dfe78fc04ca8a11e33de587
alice-cze <80326251+alice-cze@users.noreply.github.com>
2021-03-10 17:49:13 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
 input.onButtonPressed(Button.A, function () {
     strip.showRainbow(1, 360)
 })
+input.onSound(DetectedSound.Loud, function () {
+    strip.clear()
+})
+input.onButtonPressed(Button.AB, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+})
 input.onButtonPressed(Button.B, function () {
     strip.showColor(neopixel.rgb(255, 103, 23))
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    strip.clear()
+    strip.showColor(neopixel.colors(NeoPixelColors.Violet))
 })
 let strip: neopixel.Strip = null
+input.setSoundThreshold(SoundThreshold.Loud, 128)
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
 strip.setBrightness(255)
