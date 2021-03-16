e8dbdffd1629a79346d16b3ebc2372f565c367e7
alice-cze <80326251+alice-cze@users.noreply.github.com>
2021-03-16 20:15:40 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,64 @@
 input.onButtonPressed(Button.A, function () {
-    strip.showRainbow(1, 360)
+    pocitadlo += -1
+    pocitadlo = Math.constrain(pocitadlo, 0, 5)
 })
 input.onSound(DetectedSound.Loud, function () {
-    strip.clear()
+    if (pocitadlo != 0) {
+        pocitadlo = 0
+    } else {
+        pocitadlo = 1
+    }
 })
 input.onButtonPressed(Button.AB, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    pocitadlo = 5
 })
 input.onButtonPressed(Button.B, function () {
-    strip.showColor(neopixel.rgb(255, 103, 23))
+    pocitadlo += 1
+    pocitadlo = Math.constrain(pocitadlo, 0, 5)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (pocitadlo != 0) {
+        pocitadlo = 0
+    } else {
+        pocitadlo = 1
+    }
+})
+input.onPinReleased(TouchPin.P1, function () {
+    if (pocitadlo != 0) {
+        pocitadlo = 0
+    } else {
+        pocitadlo = 1
+    }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Violet))
+    pocitadlo = 1
 })
-let strip: neopixel.Strip = null
-input.setSoundThreshold(SoundThreshold.Loud, 128)
-strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+let pocitadlo = 0
+input.setSoundThreshold(SoundThreshold.Loud, 111)
+let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
 strip.setBrightness(255)
+pocitadlo = 0
 basic.forever(function () {
-    strip.rotate(1)
-    basic.pause(200)
-    strip.show()
+    if (pocitadlo == 0) {
+        strip.clear()
+    }
+    if (pocitadlo == 1) {
+        strip.showColor(neopixel.rgb(255, 103, 23))
+    }
+    if (pocitadlo == 2) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    }
+    if (pocitadlo == 3) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    }
+    if (pocitadlo == 4) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Violet))
+    }
+    if (pocitadlo == 5) {
+        strip.showRainbow(1, 360)
+        strip.rotate(1)
+        basic.pause(200)
+    }
+    basic.showNumber(pocitadlo)
 })
